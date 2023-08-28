d16df9462b294d82a40a10801c125aea13939dfa
Germán Mudry <gmudry@frro.utn.edu.ar>
2023-08-28 16:06:26 -0300

    agregado comentario

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=S, initial-scale=1.0">
+    <title>iniciando con git y visual studio code</title>
+</head>
+<body>
+    
+    <!-- Contenedor 1 -->
+    <div class="container">
+        <div class="row">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, enim.</p>
+        </div>
+    </div>
+
+</body>
+</html>
